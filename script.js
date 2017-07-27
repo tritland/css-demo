96a06a152bbdb968b9e64a8565decb4f859ecbad
Tom Ritland <t_ritland@yahoo.com>
2017-07-27 10:48:47 -0500

    adding demo files

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log('sourced');
+
+$(document).ready(function(){
+
+  $('#add').click(function(){
+    $('div').addClass('jorah');
+
+  });
+
+$('#remove').click(function(){
+  $('div').removeClass('jorah');
+
+});
+});
